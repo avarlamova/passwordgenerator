ff919f5bd7bf2ea9ade3f979291de97caaf57605
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-04-27 23:37:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 ﻿new Vue ({
     el: '#password',
-    data() {
-        return {
+    data: {
+        options: [
+            {
+                name: 'Include lowercase',
+                status: true,
+                chars: 'abcdefghjkmnopqrstuvwxyz'
+            },
+
+            {
+                name: 'Include uppercase',
+                status: false,
+                chars: 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
+            },
+            {
+                name: 'Include digits',
+                status: true,
+                chars: '0123456789'
+            },
+            {
+                name: 'Include special symbols',
+                status: false,
+                chars: '!$%&?+*#-/'
+            }
+        ],
             password: '',
             settings: {
             length: 12,
             digits: 4,
             symbols: 8,
-        }
         }
     }
 })
