f114993ea41dcf12c27f087c396ed554a502a9f8
avarlamova <59831804+avarlamova@users.noreply.github.com>
2021-08-13 23:36:50 +0300

    refactoring, more precise names

--- main.js
+++ main.js
@@ -1,76 +1,101 @@
-﻿new Vue ({
-    el: '#password',
-    data() {
-        return {
-            password: ' ',
-            refresh: true,
-            length:12,
-            checkedoption:false,
-            length:18,
-            options: [
-                {
-                    name: 'Include lowercase',
-                    checked: false,
-                },
-
-                {
-                    name: 'Include uppercase',
-                    checked: false,
-                },
-                {
-                    name: 'Include digits',
-                    checked: true,
-                },
-                {
-                    name: 'Include special symbols',
-                    checked: false,
-                }
-            ],
-        };
-    },
-     computed: {
-     generatePassword() {
-            let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-            let specialsymbols = ["!", "$", "%", "&", "?", "+", "*", "#", "-", "/"];
-            let arr = [];
-            let length = this.length;
-            let actualcount = 0;
-            [...this.options]
-                .map(item => {
-                    if (item.checked === true)
-                        actualcount = actualcount + 1;
-                })
-            if (actualcount === 0) return 'You should check at least one option!';
-            [...this.options]
-                .map(item => {
-                    for (i = 0; i < length; i++) {
-                        let random = Math.floor(Math.random() * length);
-                        if (item.name === 'Include lowercase' && item.checked === true)
-                            arr.push(letters[random]);
-                        if (item.name === 'Include uppercase' && item.checked === true)
-                            arr.push(letters[random].toUpperCase());
-                        if (item.name === 'Include digits' && item.checked === true)
-                            arr.push(Math.round(Math.random() * 9));
-                        if (item.name === 'Include special symbols' && item.checked === true)
-                            arr.push(specialsymbols[random]);
-                    }
-                })
-            return arr.sort(() => Math.random() - 0.5).join('').slice(0, length);
+﻿new Vue({
+  el: "#app",
+  data() {
+    return {
+      password: " ",
+      refresh: true,
+      checkedoption: false,
+      length: 18,
+      options: [
+        {
+          name: "Include lowercase",
+          checked: false,
         },
-    },
-      methods: {
-        copy() {
-            let copied = document.createElement("textarea");
-            copied.style.opacity = '0';
-            copied.style.position = 'fixed';
-            copied.value = this.generatePassword;
-            password.append(copied);
-            copied.select();
-            document.execCommand('copy');
-            password.removeChild(copied);
-            alert ('Copied successfully!')
-        },
-      
-    },
 
-})
+        {
+          name: "Include uppercase",
+          checked: false,
+        },
+        {
+          name: "Include digits",
+          checked: true,
+        },
+        {
+          name: "Include special symbols",
+          checked: false,
+        },
+      ],
+    };
+  },
+  computed: {
+    generatedPassword() {
+      let letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ];
+      let specialsymbols = ["!", "$", "%", "&", "?", "+", "*", "#", "-", "/"];
+
+      let arr = [];
+      let length = this.length;
+      let actualcount = 0;
+      [...this.options].map((item) => {
+        if (item.checked === true) actualcount = actualcount + 1;
+      });
+      if (actualcount === 0) return "You should check at least one option!";
+      [...this.options].map((item) => {
+        for (i = 0; i < length; i++) {
+          let random = Math.floor(Math.random() * length);
+          if (item.name === "Include lowercase" && item.checked === true)
+            arr.push(letters[random]);
+          if (item.name === "Include uppercase" && item.checked === true)
+            arr.push(letters[random].toUpperCase());
+          if (item.name === "Include digits" && item.checked === true)
+            arr.push(Math.round(Math.random() * 9));
+          if (item.name === "Include special symbols" && item.checked === true)
+            arr.push(specialsymbols[random]);
+        }
+      });
+      return arr
+        .sort(() => Math.random() - 0.5)
+        .join("")
+        .slice(0, length);
+    },
+  },
+  methods: {
+    copy() {
+      let copied = document.createElement("textarea");
+      copied.style.opacity = "0";
+      copied.style.position = "fixed";
+      copied.value = this.generatedPassword;
+      document.body.append(copied);
+      copied.select();
+      document.execCommand("copy");
+      password.removeChild(copied);
+      alert("Copied successfully!");
+    },
+  },
+});
